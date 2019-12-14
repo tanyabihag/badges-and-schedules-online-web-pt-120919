3fe89931f758cd7bddb1380908da543f6f7c8e8a
Tanya Bihag <tbihag@gmail.com>
2019-12-14 03:22:57 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,12 +1,12 @@
 def badge_maker(name)
-  return "Hello, my name is #{name}"
+  return "Hello, my name is #{name}."
 end
 
 def batch_badge_creator(attendees)
   badge_names = []
   
   attendees.each do|name|
-    badge_names << "Hello, my name is #{name}"
+    badge_names << "Hello, my name is #{name}."
   end
   return badge_names
 end
